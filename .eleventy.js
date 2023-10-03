0468cae6f45f5d526d662f5f375f4d0a2c1a83d3
Iveta Círová <cirova.iveta@ptw.cz>
2023-10-03 15:01:45 +0200

    základní struktura a konfigurace 11ty

---
+++ .eleventy.js
@@ -0,0 +1,17 @@
+module.exports=function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("images");
+    eleventyConfig.addPassthroughCopy("css");
+
+    // eleventyConfig.addPassthroughCopy("**/*.js");
+
+    // eleventyConfig.addFilter("limit", function (arr, limit) {
+    //     return arr.slice(0, limit);
+    // });
+
+    return{
+        templateFormats: ["njk", "html", "md", "liquid"],
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        dataTemplateEngine: "njk",
+    }
+};
